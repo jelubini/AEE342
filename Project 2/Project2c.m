0ba4e9db113b2bebbb9aed644bee45400e7c0eca
Joel Lubinitsky <jelubini@syr.edu>
2015-03-06 08:47:00 -0500

    last

--- Project 2/Project2c.m
+++ Project 2/Project2c.m
@@ -1,4 +1,4 @@
-function Project2c(alpha, n_pan, m, p, tt)
+function Project2c(alpha, n_pan, n_vort, m, p, tt)
     % Project2jfd - 2D potential flow via source panel method
     %    written by John Dannenhoffer for AEE342 (Spring 2015)
     %    adapted from Anderson's "Fundamentals of Aerodynamics, 5th ed"
@@ -10,7 +10,7 @@ function Project2c(alpha, n_pan, m, p, tt)
 
     close all
     
-    n_sl  =   60;                % number of streamlines
+    n_sl  =   20;                % number of streamlines
     dtbub = 0.25;                % time increment between bubbles
     xmin  = -0.5;                % mininum X in domain
     xmax  = +1.5;                % maximum X in domain
@@ -26,7 +26,6 @@ function Project2c(alpha, n_pan, m, p, tt)
     % define airfoil geometry
     zeta = linspace(pi, 2 * pi, n_pan / 2);
     xCamber = 0.5 * (1 + cos(zeta));
-%     yThickness = (tt / 0.20) * ((0.2969 * sqrt(xCamber)) - (0.1260 .* xCamber) - (0.3516 .* xCamber .^ 2) + (0.2843 .* xCamber .^ 3) - (0.1015 .* xCamber .^ 4));
     yThickness = (tt / 0.20) * ((0.2969 * sqrt(xCamber)) - (0.1260 .* xCamber) - (0.3516 .* xCamber .^ 2) + (0.2843 .* xCamber .^ 3) - (0.1036 .* xCamber .^ 4));
 
     if p > 0 && m > 0
@@ -82,15 +81,56 @@ function Project2c(alpha, n_pan, m, p, tt)
     Uinf  = cos(alpha * pi/180);
     Vinf  = sin(alpha * pi/180);
     
-        % Superposition of vortex at LE
+    % Vortex Placement
 
-    indexQuarterChord = find(xCamber <= 0.25, 1, 'last');
+%     indexRangeVortexLower = find(xCamber <= 0.1, 1, 'last');
+%     indexRangeVortexUpper = find(xCamber <= 0.4, 1, 'last');
+%     indexRangeVortex = [indexRangeVortexLower, indexRangeVortexUpper];
+%     stepVortex = (indexRangeVortex(2) - indexRangeVortex(1)) ./ (n_vort - 1);
+%     xVortex = xCamber(indexRangeVortex(1) : floor(stepVortex) : indexRangeVortex(2));
+%     yVortex = yCamber(indexRangeVortex(1) : floor(stepVortex) : indexRangeVortex(2));
     
-    xV1 = xCamber(indexQuarterChord);
-    yV1 = yCamber(indexQuarterChord);
+%     rangeVortexLower = 0.1;
+%     rangeVortexUpper = 0.4;
+%     find(xCamber >= rangeVortexLower
+    
+    if n_vort == 1
+        
+        indexQuarterChord = find(xCamber <= 0.25, 1, 'last');
+        xV1 = xCamber(indexQuarterChord);
+        yV1 = yCamber(indexQuarterChord);
+        
+    elseif n_vort == 4
+        
+        indexV1 = find(xCamber <= 0.1, 1, 'last');
+        indexV2 = find(xCamber <= 0.2, 1, 'last');
+        indexV3 = find(xCamber <= 0.3, 1, 'last');
+        indexV4 = find(xCamber <= 0.4, 1, 'last');
+        
+        xV1 = xCamber(indexV1);
+        yV1 = yCamber(indexV1);
+        xV2 = xCamber(indexV2);
+        yV2 = yCamber(indexV2);
+        xV3 = xCamber(indexV3);
+        yV3 = yCamber(indexV3);
+        xV4 = xCamber(indexV4);
+        yV4 = yCamber(indexV4);
+        
+        xV = [xV1, xV2, xV3, xV4];
+        yV = [yV1, yV2, yV3, yV4];
+        
+    else
+        error('Can only calculate vortex strengths for 1 or 4 vorticies')
+        
+    end
+        
+
+        
+    
+
 
     % find the source panel strengths
-    [xcp, ycp, Cp, lambda, strengthGamma] = sourcePanel(Uinf, Vinf, X, Y, xV1, yV1);
+    [xcp, ycp, Cp, lambda, strengthGamma] = sourcePanel(Uinf, Vinf, X, Y, xV, yV);
 
     
     % integrate pressure coefficient for force coefficients
@@ -142,9 +182,24 @@ function Project2c(alpha, n_pan, m, p, tt)
 
     % plot of configuration and streamlines (figure 2, subplot 4)
     subplot(2,2,4)
+    hold on
         plot([X, X(1)], [Y, Y(1)], '-*b')
-    
-        hold on
+        
+        if n_vort == 1
+            
+            plot(xCamber(indexQuarterChord), yCamber(indexQuarterChord), 'o', 'color', [1 0 0])
+        
+        elseif n_vort == 4
+            
+            plot(xCamber(indexV1), yCamber(indexV1), 'o', 'color', [1 0 0])
+            plot(xCamber(indexV2), yCamber(indexV2), 'o', 'color', [1 0 0])
+            plot(xCamber(indexV3), yCamber(indexV3), 'o', 'color', [1 0 0])
+            plot(xCamber(indexV4), yCamber(indexV4), 'o', 'color', [1 0 0])
+            
+        else
+            error('Can only calculate vortex strengths for 1 or 4 vorticies')
+        end
+            
         for i_sl = 1 : n_sl
             xbeg = -0.5;
             ybeg = -0.8 + (i_sl-1) / (n_sl-1) * (0.8 - -0.8);
@@ -161,7 +216,7 @@ function Project2c(alpha, n_pan, m, p, tt)
                 plot(x_bub, y_bub, 'og', 'MarkerSize', 2)
             end % if
         end % for i_sl
-        hold off
+       
     
         title(strcat('C_l =', num2str(Cl), ' C_d =', num2str(Cd)))
         xlabel('x')
@@ -235,8 +290,17 @@ function [xcp, ycp, Cp, lambda, strengthGamma] = sourcePanel(Uinf, Vinf, X, Y, x
                 Mnorm(i,j) = pi;
                 Mtang(i,j) = 0;
             elseif (j == n + 1)
-                Mnorm(i, j) = ((xV1 - xcp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2)) .* cos(phi(i)) + ((yV1 - ycp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2)) .* sin(phi(i));
+                if n_vort == 1
+                    
+                    Mnorm(i, j) = ((xV1 - xcp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2)) .* cos(phi(i)) + ((yV1 - ycp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2)) .* sin(phi(i));
                 
+                elseif n_vort == 4
+                    
+                    Mnorm(i, j) = (((xV1 - xcp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2)) + ((xV2 - xcp(i)) ./ ((xcp(i) - xV2) .^ 2 + (ycp(i) - yV2).^ 2)) + ((xV3 - xcp(i)) ./ ((xcp(i) - xV3) .^ 2 + (ycp(i) - yV3).^ 2)) + ((xV4 - xcp(i)) ./ ((xcp(i) - xV4) .^ 2 + (ycp(i) - yV4).^ 2))) .* cos(phi(i)) + (((yV2 - ycp(i)) ./ ((xcp(i) - xV2) .^ 2 + (ycp(i) - yV2).^ 2)) + ((yV3 - ycp(i)) ./ ((xcp(i) - xV3) .^ 2 + (ycp(i) - yV3).^ 2)) + ((yV4 - ycp(i)) ./ ((xcp(i) - xV4) .^ 2 + (ycp(i) - yV4).^ 2)) + ((yV1 - ycp(i)) ./ ((xcp(i) - xV1) .^ 2 + (ycp(i) - yV1).^ 2))) .* sin(phi(i));
+                    
+                else
+                    error('Can only calculate vortex strengths for 1 or 4 vorticies')
+                end
             else
                 A = - (xcp(i) - X(j)) * cos(phi(j)) - (ycp(i) - Y(j)) * sin(phi(j));
                 B =   (xcp(i) - X(j)) ^ 2           + (ycp(i) - Y(j)) ^ 2;
@@ -265,9 +329,26 @@ function [xcp, ycp, Cp, lambda, strengthGamma] = sourcePanel(Uinf, Vinf, X, Y, x
         
     end
     
-    Mnorm(n + 1, n + 1) = ((xV1 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV1) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* cos(phi(n ./ 2))...
-                        + ((xV1 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV1) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* cos(phi(n ./ 2 + 1));
-    
+    if n_vort == 1
+        
+        Mnorm(n + 1, n + 1) = ((xV1 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV1) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* cos(phi(n ./ 2))...
+                            + ((xV1 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV1) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* cos(phi(n ./ 2 + 1))...
+                       
+    elseif n_vort == 4
+        
+        Mnorm(n + 1, n + 1) = ((xV1 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV1) ./ ((xcp(n ./ 2) - xV1) .^ 2 + (ycp(n ./ 2) - yV1).^ 2)) .* cos(phi(n ./ 2))...
+                            + ((xV1 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV1) ./ ((xcp(n ./ 2 + 1) - xV1) .^ 2 + (ycp(n ./ 2 + 1) - yV1).^ 2)) .* cos(phi(n ./ 2 + 1))...
+                            + ((xV2 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV2) .^ 2 + (ycp(n ./ 2) - yV2).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV2) ./ ((xcp(n ./ 2) - xV2) .^ 2 + (ycp(n ./ 2) - yV2).^ 2)) .* cos(phi(n ./ 2))...
+                            + ((xV2 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV2) .^ 2 + (ycp(n ./ 2 + 1) - yV2).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV2) ./ ((xcp(n ./ 2 + 1) - xV2) .^ 2 + (ycp(n ./ 2 + 1) - yV2).^ 2)) .* cos(phi(n ./ 2 + 1))...
+                            + ((xV3 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV3) .^ 2 + (ycp(n ./ 2) - yV3).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV3) ./ ((xcp(n ./ 2) - xV3) .^ 2 + (ycp(n ./ 2) - yV3).^ 2)) .* cos(phi(n ./ 2))...
+                            + ((xV3 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV3) .^ 2 + (ycp(n ./ 2 + 1) - yV3).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV3) ./ ((xcp(n ./ 2 + 1) - xV3) .^ 2 + (ycp(n ./ 2 + 1) - yV3).^ 2)) .* cos(phi(n ./ 2 + 1))...
+                            + ((xV4 - xcp(n ./ 2)) ./ ((xcp(n ./ 2) - xV4) .^ 2 + (ycp(n ./ 2) - yV4).^ 2)) .* sin(phi(n ./ 2)) + ((ycp(n ./ 2) - yV4) ./ ((xcp(n ./ 2) - xV4) .^ 2 + (ycp(n ./ 2) - yV4).^ 2)) .* cos(phi(n ./ 2))...
+                            + ((xV4 - xcp(n ./ 2 + 1)) ./ ((xcp(n ./ 2 + 1) - xV4) .^ 2 + (ycp(n ./ 2 + 1) - yV4).^ 2)) .* sin(phi(n ./ 2 + 1)) + ((ycp(n ./ 2 + 1) - yV4) ./ ((xcp(n ./ 2 + 1) - xV4) .^ 2 + (ycp(n ./ 2 + 1) - yV4).^ 2)) .* cos(phi(n ./ 2 + 1));
+        
+    else
+        error('Can only calculate vortex strengths for 1 or 4 vorticies')
+    end
+                        
     RHS(n + 1) = -2 * pi * (Vinf * sin(phi(n ./ 2)) + Uinf * cos(phi(n ./ 2)))...
                + -2 * pi * (Vinf * sin(phi(n ./ 2 + 1)) + Uinf * cos(phi(n ./ 2 + 1)));
 
@@ -276,7 +357,19 @@ function [xcp, ycp, Cp, lambda, strengthGamma] = sourcePanel(Uinf, Vinf, X, Y, x
     strengthGamma = lambda(end)
     lambda = lambda(1 : n);
     % compute the tangential velocities and hence the Cp
-    V = (Uinf + (strengthGamma ./ (2 .* pi)) .* (ycp - yV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) .* cos(phi) + (Vinf + (-strengthGamma ./ (2 .* pi)) .* (xcp - xV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) .* sin(phi);
+    
+    if n_vort == 1
+        
+        V = (Uinf + (strengthGamma ./ (2 .* pi)) .* (ycp - yV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) .* cos(phi) + (Vinf + (-strengthGamma ./ (2 .* pi)) .* (xcp - xV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) .* sin(phi);
+        
+    elseif n_vort == 4
+        
+        V = (Uinf + (strengthGamma ./ (2 .* pi)) .* ((ycp - yV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) + (ycp - yV2)./ ((xcp - xV2) .^ 2 + (ycp - yV2).^ 2)) + (ycp - yV3)./ ((xcp - xV3) .^ 2 + (ycp - yV3).^ 2)) + (ycp - yV4)./ ((xcp - xV4) .^ 2 + (ycp - yV4).^ 2))) .* cos(phi) + (Vinf + (-strengthGamma ./ (2 .* pi)) .* ((xcp - xV1)./ ((xcp - xV1) .^ 2 + (ycp - yV1).^ 2)) + (xcp - xV2)./ ((xcp - xV2) .^ 2 + (ycp - yV2).^ 2)) + (xcp - xV3)./ ((xcp - xV3) .^ 2 + (ycp - yV3).^ 2)) + (xcp - xV4)./ ((xcp - xV4) .^ 2 + (ycp - yV4).^ 2))) .* sin(phi);
+
+    else
+        error('Can only calculate vortex strengths for 1 or 4 vorticies')
+    end
+    
     for j = 1 : n
         V = V + lambda(j) ./ (2*pi) .* Mtang(:,j)';
     end % for
@@ -299,12 +392,23 @@ end % function sourcePanel
 
 %--------------------------------------------------------------------
 
-function u = Uvel(x, y, Uinf, X, Y, lambda, strengthGamma, xV1, yV1)
+function u = Uvel(x, y, Uinf, X, Y, lambda, strengthGamma, xV, yV, n_vort)
     % Uvel - x-component of velocity at (x,y)
 
     % uniform flow
-    u = (Uinf + (strengthGamma ./ (2 .* pi)) .* (y - yV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) .* ones(size(x)) ;
+    
+    if n_vort == 1
+        
+        u = (Uinf + (strengthGamma ./ (2 .* pi)) .* (y - yV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) .* ones(size(x)) ;
 
+    elseif n_vort == 4
+        
+        u = (Uinf + (strengthGamma ./ (2 .* pi)) .* ((y - yV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) + (y - yV2)./ ((x - xV2) .^ 2 + (y - yV2).^ 2)) +(y - yV3)./ ((x - xV3) .^ 2 + (y - yV3).^ 2)) + (y - yV4)./ ((x - xV4) .^ 2 + (y - yV4).^ 2)) .* ones(size(x)) ;
+        
+    else
+                error('Can only calculate vortex strengths for 1 or 4 vorticies')
+    end
+    
     % loop through source panels
     n = length(lambda);
     for j = 1 : n
@@ -330,12 +434,21 @@ end % function Uvel
 
 %--------------------------------------------------------------------
 
-function v = Vvel(x, y, Vinf, X, Y, lambda, strengthGamma, xV1, yV1)
+function v = Vvel(x, y, Vinf, X, Y, lambda, strengthGamma, xV, yV, n_vort)
     % Vvel - y-component of velocity at (x,y)
 
     % uniform flow
-    v = (Vinf + (-strengthGamma ./ (2 .* pi)) .* (x - xV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) .* ones(size(x));
+    if n_vort == 1
+        
+        v = (Vinf + (-strengthGamma ./ (2 .* pi)) .* (x - xV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) .* ones(size(x));
 
+    elseif n_vort == 4
+        
+                v = (Vinf + (-strengthGamma ./ (2 .* pi)) .* ((x - xV1)./ ((x - xV1) .^ 2 + (y - yV1).^ 2)) + (x - xV2)./ ((x - xV2) .^ 2 + (y - yV2).^ 2)) + (x - xV3)./ ((x - xV3) .^ 2 + (y - yV3).^ 2)) + (x - xV4)./ ((x - xV4) .^ 2 + (y - yV4).^ 2)) .* ones(size(x));
+    else
+                error('Can only calculate vortex strengths for 1 or 4 vorticies')
+    end
+    
     % loop through source panels
     n = length(lambda);
     for j = 1 : n
@@ -361,7 +474,7 @@ end % function Vvel
 
 %--------------------------------------------------------------------
 
-function uv = strmLine(t, xy, Uinf, Vinf, X, Y, lambda, strengthGamma, xV1, yV1)
+function uv = strmLine(t, xy, Uinf, Vinf, X, Y, lambda, strengthGamma, xV, yV)
     % strmLine - vector velocity function (used in ode45)
 
     % extract the x and y coordinates
@@ -369,8 +482,8 @@ function uv = strmLine(t, xy, Uinf, Vinf, X, Y, lambda, strengthGamma, xV1, yV1)
     y = xy(2);
 
     % get the velocities and return them as a vector
-    uv = [Uvel(x, y, Uinf, X, Y, lambda, strengthGamma, xV1, yV1); ...
-         Vvel(x, y, Vinf, X, Y, lambda, strengthGamma, xV1, yV1)];
+    uv = [Uvel(x, y, Uinf, X, Y, lambda, strengthGamma, xV, yV); ...
+         Vvel(x, y, Vinf, X, Y, lambda, strengthGamma, xV, yV)];
 end % function strmLine
   
 %--------------------------------------------------------------------
